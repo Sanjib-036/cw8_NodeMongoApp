f72e4af75bc6ef1538c324b7daa265ed5ad3f3f7
Alexander Supertramp <Sanjib-036@github.com>
2020-12-29 10:07:31 +0600

    db connection

--- server.js
+++ server.js
@@ -3,9 +3,21 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
 var server = http.Server(app)
+var Article = require('./article.model')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+//DB connection
+
+var mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+var dbURL = 'mongodb://localhost:27017/cw10' //change this if you are using Atlas
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connection.on('error', function (err) {
+    console.log(err)
+})
+
 
 // your server routes go here
 app.get('/', function (request, response) {
@@ -25,19 +37,30 @@ var articles = [{ title: 'test', content: 'test' },
 { title: 'test2', content: 'test2' }];
 app.post('/article/new', function (request, response) {
     //console.log('post request');
+    var newArticle = new Article(request.body)
+    newArticle.save(function (err, data) {
+        if (err)
+            return response.status(400).json({
+                error: 'Title is missing'
+            })
+        return response.status(200).json({
+            message: 'Article created successfully'
+        })
+    })
 
-    let newArticle = request.body;
-    if (newArticle.name) {
-        articles.push(request.body);
-        console.log(articles)
-        response.json(articles);
 
-    }
-    else {
-        response.status(400).json({
-            msg: 'No name Added'
-        });
-    }
+    // let newArticle = request.body;
+    // if (newArticle.name) {
+    //     articles.push(request.body);
+    //     console.log(articles)
+    //     response.json(articles);
+
+    // }
+    // else {
+    //     response.status(400).json({
+    //         msg: 'No name Added'
+    //     });
+    // }
 
 
 })
@@ -49,13 +72,20 @@ app.get('/article/:id', function (request, response) {
     })
 })
 app.get('/articles/all', function (request, response) {
-    response.render('secondArticle.ejs', {
-        articles: articles
+    Article.find({}, function (err, data) {
+        response.render('secondArticle.ejs', {
+            articles: data
+        })
     })
+
+
+    // response.render('secondArticle.ejs', {
+    //     articles: articles
+    // })
 })
 
 server.listen(process.env.PORT || 3000,
     process.env.IP || 'localhost', function () {
         console.log('Server running');
     })
-module.exports = { app, server }
+module.exports = { app, server, mongoose }
